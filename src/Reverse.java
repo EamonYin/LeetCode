1c97354c0803575ecbf23e02cfaf402e6153f18a
LAPTOP-G60O641F <yiming_102@163.com>
2020-04-07 15:19:38 +0800

    7. 整数反转

--- src/Reverse.java
+++ src/Reverse.java
@@ -4,21 +4,26 @@
  * 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
  */
 public class Reverse {
+
     public static int reverse(int x) {
-        int [] arr = new int[32];
+        //int类型能存放10位的数字，最大为2147483647；
         int rev = 0;
-        int newnum = 0;
-        while(x != 0){
-            rev = rev *10 + x % 10;//得到个位翻10倍累加下一次的个位，形成倒序
-            x = x/10;//去除个位后
-        }
+
+            while (x != 0) {
+                int old = rev;//记录上一次循环的rev
+                System.out.println("----"+rev+"~~~~~"+Integer.MAX_VALUE);
+                rev = rev * 10 + x % 10;//得到个位翻10倍累加下一次的个位，形成倒序
+                System.out.println("rev" + rev);
+                x = x / 10;//去除个位后
+                if(rev/10 != old) return 0;
+            }
         return rev;
     }
 
-    public static void main(String[] args) {
-        int num = 123;
+     public static void main(String[] args) {
+        int num = 1534236469;
         int reverse = reverse(num);
-        System.out.println("原数字："+num);
-        System.out.println("倒叙后："+reverse);
+        System.out.println("原数字：" + num);
+        System.out.println("倒叙后：" + reverse);
     }
 }
